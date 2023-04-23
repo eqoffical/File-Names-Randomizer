0ea2f81dccc6b86aa27bf6406bf6146f953efc7b
RedkoDev <redko0111@gmail.com>
2023-04-23 22:35:35 +0300

    fixed bugs

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ if confirmation.lower() == "y":
     # Main (it's renaming all the files)
     os.chdir(path)
     for filename in os.listdir(path):
-        new_name = ''.join(random.choices(string.ascii_letters + string.digits, letters=10))    # Generate a random name
-        file_extension = os.path.splitext(filename)[1]                                          # Get the file extension
-        new_filename = new_name + file_extension                                                # Create the new file name by concatenating the random name and file extension
-        os.rename(os.path.join(path, filename), os.path.join(path, new_filename))               # Rename the file
-        print("Done, all files are renamed.")
+        new_name = ''.join(random.choices(string.ascii_letters + string.digits, k=10))    # Generate a random name
+        file_extension = os.path.splitext(filename)[1]                                    # Get the file extension
+        new_filename = new_name + file_extension                                          # Create the new file name by concatenating the random name and file extension
+        os.rename(os.path.join(path, filename), os.path.join(path, new_filename))         # Rename the file
+    print("Done, all files are renamed.")
 
 #If no
 elif confirmation.lower() == "n": exit()
